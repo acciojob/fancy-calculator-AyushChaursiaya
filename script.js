5bb1f765c0af36de1ae3ec2e91824c1296a67728
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2023-12-16 22:20:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 // Take input for the first variable and multiply it by 2
+// var firstInput = prompt("Enter the first number:");
+// var multipliedResult = firstInput * 2;
+
+// // Take input for the second variable
+// var secondInput = prompt("Enter the second number:");
+
+// // Add the second input to the result of the first input multiplied by 2
+// var result = multipliedResult + parseInt(secondInput);
+
+// // Display the final result using alert() in the specified format
+// alert("The final result is: " + result);
+
+
 var firstInput = prompt("Enter the first number:");
-var multipliedResult = firstInput * 2;
-
-// Take input for the second variable
+var multiply = firstInput * 2;
 var secondInput = prompt("Enter the second number:");
-
-// Add the second input to the result of the first input multiplied by 2
-var result = multipliedResult + parseInt(secondInput);
-
-// Display the final result using alert() in the specified format
+var result = multiply + parseInt(secondInput);
 alert("The final result is: " + result);
+
